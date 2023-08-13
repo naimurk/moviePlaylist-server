cb89466119697262a41c839d3b873084a639c6ef
naimurk <naimurk781@gmail.com>
2023-08-13 20:06:31 +0600

    second time commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://moviePlaylist:GeWcLWBQh6FWX5MX@cluster0.50l1tkw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.50l1tkw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
